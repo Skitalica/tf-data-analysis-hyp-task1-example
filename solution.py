129c27e89f911d06e1605fe556a37cf92d017a28
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-10 13:14:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import numpy as np
-
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+import math
+from scipy import stats as st
+chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=0.02
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    
+    z = (p1 - p2) / math.sqrt(p * (1 - p) * (1/x_cnt + 1/y_cnt))
+    distr = st.norm(0, 1) 
+    p_value = (1 - distr.cdf(abs(z))) * 2
+
+    if (p_value < alpha):
+      return True
+    else: 
+      return False
